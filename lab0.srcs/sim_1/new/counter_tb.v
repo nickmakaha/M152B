f3550252c36f3cf85b3237bf48a3bdcaa46f51d6
Nicholas Johnson <36455331+nickmakaha@users.noreply.github.com>
2024-10-02 10:25:39 -0700

    Add files via upload

--- lab0.srcs/sim_1/new/counter_tb.v
+++ lab0.srcs/sim_1/new/counter_tb.v
@@ -21,32 +21,44 @@
 
 
 module counter_tb();
-reg clock;
-reg reset;
-reg enable;
-wire [3:0] counter;
-counter c0 (.clock(clock),
- .reset(reset),
- .enable(enable),
- .counter(counter));
-
-always #5 clock = ~clock;
-
-initial begin
-    reset <= 0;
-    clock <= 0;
-    enable <= 1;
+    reg clock;
+    reg reset;
+    reg enable;
+    wire [3:0] counter;
+    counter c0 (
+        .clock(clock),
+        .reset(reset),
+        .enable(enable),
+        .counter(counter)
+    );
     
-    #375 enable <= 0;
-    reset <= 1;
-    
-    #50
-    enable <= 1;
-    #25
-    reset <= 0;
-end
+    always #5 clock = ~clock;
 
-
-
-    
+    initial begin
+        // Initial Testing: Counter should increment and 
+        // overflow back to zero on incrementing 15
+        reset <= 0;
+        clock <= 0;
+        enable <= 1;
+        
+        // Test Hold on Enable
+        #375 
+        enable <= 0;
+        
+        // Test Reset Functionality - Counter should go to zero
+        #50 
+        enable <= 1;
+        reset <= 1;
+        
+        // Normal Functionality
+        #25
+        reset <= 0;
+        enable <= 1;
+        
+        // Reset On and Enable Off at the Same time
+        // Output should stay at zero here
+        #25
+        reset <= 1;
+        enable <= 0;
+    end
 endmodule
